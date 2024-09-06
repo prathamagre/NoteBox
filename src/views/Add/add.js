ceced53cec6bd0ddc4eebe02e087c71840308f3d
prathamagre <prathamvaishu@gmail.com>
2024-09-06 14:04:29 +0530

    form handling applied

--- src/views/Add/add.js
+++ src/views/Add/add.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './add.css'
 import HomeButton from '../../components/HomeButton/HomeButton'
 
-function add() {
+function Add() {
+  const [title, setTitle] = useState("");
+
   return (
     <div>
-    <div className="secondary">
-      <h2 className="text-center highlight">📝Add Note</h2><hr></hr>
+      <div className="secondary">
+        <h2 className="text-center highlight">📝Add Note</h2><hr></hr>
+      </div>
+
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setTitle(e.target.value);
+        }}
+      />
+      <button 
+      type="button" 
+      onClick={()=>{
+        setTitle("");
+      }}>
+        clear
+      </button>
+
+      <HomeButton />
     </div>
-    <HomeButton/>
-    </div>
-  )
+  );
 }
 
-export default add
+export default Add

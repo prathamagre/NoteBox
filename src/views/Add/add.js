4f30841b6c72d61f9d0ab0f4b6715ce1659c67ae
prathamagre <prathamvaishu@gmail.com>
2024-09-06 16:57:37 +0530

    user input section added

--- src/views/Add/add.js
+++ src/views/Add/add.js
@@ -1,9 +1,14 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import './add.css'
 import HomeButton from '../../components/HomeButton/HomeButton'
+import EmojiPicker, { Emoji } from 'emoji-picker-react';
 
 function Add() {
   const [title, setTitle] = useState("");
+  const [decription, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [emoji, setEmoji] = useState("");
+  const [openEmojiDialog, setOpenEmojiDialog] = useState(false);
 
   return (
     <div>
@@ -19,14 +24,58 @@ function Add() {
           console.log(e.target.value);
           setTitle(e.target.value);
         }}
+        className="user-input"
       />
-      <button 
-      type="button" 
-      onClick={()=>{
-        setTitle("");
-      }}>
-        clear
-      </button>
+
+      <input
+        type="text"
+        placeholder="Description"
+        value={decription}
+        onChange={(e) => {
+          console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+        className="user-input"
+      />
+
+      <select className="user-input user-input-category">
+        <option>category</option>
+        <option value="shopping">Shopping</option>
+        <option value="health">Health</option>
+        <option value="learning">Learning</option>
+        <option value="personal">Personal</option>
+        <option value="work">Work</option>
+      </select>
+
+      <div className="user-input" onClick={() => setOpenEmojiDialog(true)}
+       >
+        {emoji? emoji:"Select Emoji"}
+      </div>
+
+      <EmojiPicker
+          open={openEmojiDialog}
+          height={"400px"}
+          Theme={"dark"}
+          onEmojiClick={(emojiObject) => {
+            setEmoji(emojiObject.emoji)
+            setOpenEmojiDialog(false)
+          }}
+          className="emoji-picker"/>
+
+      <div className="addpge-btn-container">
+        <button
+          type="button"
+          onClick={() => {
+            setTitle("");
+            setCategory("");
+            setDescription("");
+            setEmoji("");
+          }}
+          className="action-btn">
+          Add Note
+        </button>
+        
+      </div>
 
       <HomeButton />
     </div>
